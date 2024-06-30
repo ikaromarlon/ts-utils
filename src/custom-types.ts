01f21310d24544bcb7391e06a72f485157c8a8d6
Icaro Silva <ikaromarlon@gmail.com>
2024-06-30 15:50:33 +0200

    feat: add custom-types

---
+++ src/custom-types.ts
@@ -0,0 +1,20 @@
+export type NoTraceableProps<T> = Omit<
+  T,
+  | 'createdAt'
+  | 'createdBy'
+  | 'updatedAt'
+  | 'updatedBy'
+  | 'deletedAt'
+  | 'deletedBy'
+>;
+
+export type NoPersistenceProps<T> = Omit<NoTraceableProps<T>, 'id'>;
+
+export type NoFunctionProps<T> = Pick<
+  T,
+  {
+    [K in keyof T]: T[K] extends Function ? never : K;
+  }[keyof T]
+>;
+
+export type RawObject<T> = NoFunctionProps<NoPersistenceProps<T>>;
